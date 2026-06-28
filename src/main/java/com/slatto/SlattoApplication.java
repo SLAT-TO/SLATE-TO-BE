a848102f42a01d0f9c7de0657f919a4b57fe4c1d
chazy-d <my0710my@gmail.com>
2026-06-29 05:21:44 +0900

    start: Spring Boot 프로젝트 초기화

---
+++ src/main/java/com/slatto/SlattoApplication.java
@@ -0,0 +1,13 @@
+package com.slatto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlattoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlattoApplication.class, args);
+	}
+
+}
