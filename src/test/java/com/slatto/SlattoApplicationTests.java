d744ff7684419ed13a99f8b7331c7a3e4b3fbd39
이상원 <sangwon020214@gmail.com>
2026-07-18 17:43:28 +0900

    test: 컨텍스트 로드 테스트 정상화
    기존에는 DataSource/JPA 오토컨피그를 제외한 채 @EnableJpaAuditing이 동작해
    'JPA metamodel must not be empty'로 contextLoads가 실패하고 있었다.
    
    - 테스트 런타임에 H2 추가, 제외 설정을 걷어내고 실제 JPA로 기동
    - ddl-auto=create-drop으로 엔티티 매핑까지 스모크 테스트가 검증하도록 변경
    - 리포지토리 MockitoBean 제거

--- src/test/java/com/slatto/SlattoApplicationTests.java
+++ src/test/java/com/slatto/SlattoApplicationTests.java
@@ -1,23 +1,10 @@
 package com.slatto;
 
-import com.slatto.domain.auth.repository.RefreshTokenRepository;
-import com.slatto.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest(properties = {
-	"spring.autoconfigure.exclude="
-		+ "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
-		+ "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
-})
+@SpringBootTest
 class SlattoApplicationTests {
-
-	@MockitoBean
-	private UserRepository userRepository;
-
-	@MockitoBean
-	private RefreshTokenRepository refreshTokenRepository;
 
 	@Test
 	void contextLoads() {
