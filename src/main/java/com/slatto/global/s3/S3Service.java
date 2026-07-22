ffff99c440e9f900cebb2eee0922cb1a7b670418
young <ljy040206@gmail.com>
2026-07-23 03:00:45 +0900

    fix: 코드 리뷰 수정 (#54)

--- src/main/java/com/slatto/global/s3/S3Service.java
+++ src/main/java/com/slatto/global/s3/S3Service.java
@@ -6,9 +6,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.time.Duration;
 import java.util.UUID;
 
 @Service
@@ -16,19 +21,19 @@ import java.util.UUID;
 public class S3Service {
 
     private final S3Client s3Client;
+    private final S3Presigner s3Presigner;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    private static final String S3_URL_FORMAT =
-            "https://%s.s3.ap-northeast-2.amazonaws.com/%s";
-
-
     public String upload(MultipartFile file) {
+
+        validateFileSize(file);
 
         String key = createKey(file);
 
-        try {
+        try (InputStream inputStream = file.getInputStream()) {
+
             PutObjectRequest request = PutObjectRequest.builder()
                     .bucket(bucket)
                     .key(key)
@@ -37,28 +42,61 @@ public class S3Service {
 
             s3Client.putObject(
                     request,
-                    RequestBody.fromBytes(file.getBytes())
+                    RequestBody.fromInputStream(
+                            inputStream,
+                            file.getSize()
+                    )
             );
 
         } catch (IOException e) {
             throw new RuntimeException("파일 업로드에 실패했습니다.");
         }
 
-        return String.format(
-                S3_URL_FORMAT,
-                bucket,
-                key
-        );
+        return createPresignedUrl(key);
     }
 
+    private String createPresignedUrl(String key) {
+
+        GetObjectRequest getObjectRequest =
+                GetObjectRequest.builder()
+                        .bucket(bucket)
+                        .key(key)
+                        .build();
+
+        GetObjectPresignRequest presignRequest =
+                GetObjectPresignRequest.builder()
+                        .signatureDuration(Duration.ofMinutes(10))
+                        .getObjectRequest(getObjectRequest)
+                        .build();
+
+        return s3Presigner.presignGetObject(presignRequest)
+                .url()
+                .toString();
+    }
 
     private String createKey(MultipartFile file) {
 
-        String originalFilename = file.getOriginalFilename();
+        String extension = "";
+
+        String filename = file.getOriginalFilename();
+
+        if (filename != null && filename.contains(".")) {
+            extension = filename.substring(filename.lastIndexOf("."));
+        }
 
         return "images/"
                 + UUID.randomUUID()
-                + "-"
-                + originalFilename;
+                + extension;
+    }
+
+    private void validateFileSize(MultipartFile file) {
+
+        long maxSize = 10 * 1024 * 1024;
+
+        if (file.getSize() > maxSize) {
+            throw new IllegalArgumentException(
+                    "파일 크기는 10MB 이하만 가능합니다."
+            );
+        }
     }
 }
